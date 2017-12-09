22311e5ef445fd668e727ed06d1060cc4b6debe1
Ron Dekker <RedKenrok@gmail.com>
2017-12-09 02:33:30 +0100

    Initial project commit

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
-# node-audiorecording
-Audio recording for node, delivers a 16-bit signed-integer linear pulse modulation WAV stream.
+# Audio recorder
+Audio recorder for [Node.js](https://nodejs.org/), delivers a 16-bit signed-integer linear pulse modulation WAV stream. Based of [Gilles De Mey](https://github.com/gillesdemey)'s [node-record-lpcm16](https://github.com/gillesdemey/node-record-lpcm16).
+
+## Installation
+```
+npm install --save node-audiorecorder
+```
+
+## Dependencies
+This module requires you to install [SoX](http://sox.sourceforge.net/) and it must be available in your $PATH.
+
+### For Linux
+```
+sudo apt-get install sox libsox-fmt-all
+```
+
+### For MacOS
+```
+brew install sox
+```
+
+### For Windows
+[Download the binaries](http://sourceforge.net/projects/sox/files/latest/download)
+
+## Usage
+
+### Constructor
+```javascript
+// Import module.
+const AudioRecorder = require('node-audiorecorder');
+
+// Options is an optional parameter for the constructor call.
+// If an option is not given the default value, as seen below, will be used.
+const options = {
+  channels: '1',			// Amount of channels to record.
+  device: null,			// Recording device to use.
+  program: 'rec',			// Which program to use, either 'arecord', 'rec', or 'sox'.
+  sampleRate: 16000,		// Audio sample rate in hz.
+  silence: '2',			// Time of silence in seconds before it stops recording.
+  threshold: 0.5,			// Silence threshold (only for 'rec' and 'sox').
+  thresholdStart: null,	// Silence threshold to start recording, overrides threshold (only for 'rec' and 'sox').
+  thresholdEnd: null,		// Silence threshold to end recording, overrides threshold (only for 'rec' and 'sox').
+};
+// Optional parameter intended for debugging.
+// The object has to implement a log and warn function.
+const logger = {
+  log: console.log,
+  warn: console.warn
+};
+
+// Create an instance.
+let audioRecorder = new AudioRecorder(options, logger);
+```
+
+> 'arecord' might not work on all operating systems. If you can't capture any sound with 'arecord', try to change device to 'arecord -l'.
+
+### Methods
+```javascript
+// Creates and starts the recording process.
+audioRecorder.Start();
+// Stops and removes the recording process.
+audioRecorder.Stop();
+// Stops the recording process and pauses the stream.
+audioRecorder.Pause();
+// Starts the recording process and resumes the stream.
+audioRecorder.Resume();
+// Returns the stream of the recording process.
+audioRecorder.Stream();
+```
+
+### Example
+```javascript
+// Imports modules.
+const fs = require('fs');
+const AudioRecorder = require('node-audiorecorder');
+
+// Initialize recorder and file stream.
+let audioRecorder = new AudioRecorder(null, console);
+let file = fs.createWriteStream('recording.wav', { encoding: 'binary' });
+
+// Start and write to the file.
+audioRecorder.start().stream().pipe(file);
+```
+
+> For another example see the [node-hotworddetector](https://github.com/RedKenrok/node-hotworddetector) module.
---
+++ index.js
@@ -0,0 +1,189 @@
+'use strict';
+
+const processSpawn = require('child_process').spawn;
+
+// Default recording values.
+const options = {
+	channels: '1',			// Amount of channels to record.
+	device: null,			// Recording device to use.
+	program: 'rec',			// Which program to use, either 'arecord', 'rec', or 'sox'.
+	sampleRate: 16000,		// Audio sample rate in hz.
+	silence: '2',			// Time of silence in seconds before it stops recording.
+	threshold: 0.5,			// Silence threshold (only for 'rec' and 'sox').
+	thresholdStart: null,	// Silence threshold to start recording, overrides threshold (only for 'rec' and 'sox').
+	thresholdEnd: null,		// Silence threshold to end recording, overrides threshold (only for 'rec' and 'sox').
+};
+
+let process,
+	stream;
+
+class AudioRecorder {
+	/**
+	 * Constructor of AudioRecord class.
+	 * @param {*} options Object with optional options variables
+	 * @param {*} logger Object with log, warn, and error functions
+	 * @returns this
+	 */
+	constructor(options, logger) {
+		this.options = Object.assign(defaults, options);
+		this.logger = logger;
+		
+		this.command = {
+			arguments: [
+				// Show no progress
+				'-q',
+				// Sample rate
+				'-r', this.options.sampleRate,
+				// Channels
+				'-c', this.options.channels
+			],
+			options: {
+				encoding: 'binary'
+			}
+		};
+		switch (this.options.program) {
+			default:
+			case 'sox':
+			case 'rec':
+				this.command.arguments.push(
+					// Sample encoding
+					'-e', 'signed-integer',
+					// Precision in bits
+					'-b', '16',
+					// Audio type
+					'-t', 'wav',
+					// Pipe
+					'-',
+					// End on silence
+					'silence','1', '0.1', (this.options.thresholdStart || this.options.threshold) + '%',
+					'1', this.options.silence, (this.options.thresholdEnd || this.options.threshold) + '%'
+				);
+				break;
+			case 'arecord':
+				this.command.arguments.push(
+					// Audio type
+					'-t', 'wav',
+					// Sample format
+					'-f', 'S16_LE',
+					// Pipe
+					'-'
+				);
+				if (this.options.device) {
+					this.command.arguments.push('-D', this.options.device)
+				}
+				break;
+		}
+		if (this.options.device) {
+			this.command.options.env = Object.assign({}, process.env, {  AUDIODEV: this.options.device });
+		}
+		
+		if (this.logger) {
+			this.logger.log('AudioRecorder command: ' + this.options.program + ' ' + this.command.arguments.join(' '));
+		}
+		
+		return this;
+	}
+	/**
+	 * Creates and starts the audio recording process.
+	 * @returns this
+	 */
+	start() {
+		if (process) {
+			if (this.logger) {
+				this.logger.warn('AudioRecorder: Process already active, killed old one started new process.');
+			}
+			process.kill();
+		}
+		
+		// Create new child process and give the recording commands.
+		process = processSpawn(this.options.program, this.command.arguments, this.command.options);
+		
+		if (this.logger) {
+			this.logger.log('AudioRecorder: Started recording.');
+		}
+		
+		return this;
+	}
+	/**
+	 * Stops and removes the audio recording process.
+	 * @returns this
+	 */
+	stop() {
+		if (!process) {
+			if (this.logger) {
+				this.logger.warn('AudioRecorder: Unable to stop recording, no process active.');
+			}
+			return this;
+		}
+		
+		process.kill();
+		process = null;
+		
+		if (this.logger) {
+			this.logger.log('AudioRecorder: Stopped recording.');
+		}
+		
+		return this;
+	}
+	/**
+	 * Stops the audio recording process and pauses the stream.
+	 * @returns this
+	 */
+	pause() {
+		if (!process) {
+			if (this.logger) {
+				this.logger.warn('AudioRecorder: Unable to pause recording, no process active.');
+			}
+			return this;
+		}
+		
+		process.kill('SIGSTOP');
+		process.stdout.pause();
+		
+		if (this.logger) {
+			this.logger.log('AudioRecorder: Paused recording.');
+		}
+		
+		return this;
+	}
+	/**
+	 * Starts the audio recording process and resumes the stream.
+	 * @returns this
+	 */
+	resume() {
+		if (!process) {
+			if (this.logger) {
+				this.logger.warn('AudioRecorder: Unable to resumed recording, started recording automaticly.');
+			}
+			return this.start();
+		}
+		
+		process.kill('SIGCONT');
+		process.stdout.resume();
+		
+		if (this.logger) {
+			this.logger.log('AudioRecorder: Resumed recording.');
+		}
+		
+		return this;
+	}
+	/**
+	 * Returns the stream of the audio recording process.
+	 */
+	stream() {
+		if (!stream) {
+			if (!process) {
+				if (this.logger) {
+					this.logger.warn('AudioRecorder: Unable to retrieve stream, because no process not active. Call the start or resume function first.');
+				}
+				return null;
+			}
+			
+			stream = process.stdout;
+		}
+		
+		return stream;
+	}
+}
+
+module.exports = AudioRecorder;
